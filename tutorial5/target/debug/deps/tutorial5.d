49b29fb64c8f06522587583fe7be0e915148d42a
SaadTanveer14 <saad.tanveer11400@gmail.com>
2024-01-04 10:20:33 +0500

    tutorial5

---
+++ tutorial5/target/debug/deps/tutorial5.d
@@ -0,0 +1,5 @@
+D:\Rust Practice\tutorial5\target\debug\deps\tutorial5.exe: src\main.rs
+
+D:\Rust Practice\tutorial5\target\debug\deps\tutorial5.d: src\main.rs
+
+src\main.rs:
---
+++ tutorial5/target/debug/tutorial5.d
@@ -0,0 +1,1 @@
+D:\Rust\ Practice\tutorial5\target\debug\tutorial5.exe: D:\Rust\ Practice\tutorial5\src\main.rs
